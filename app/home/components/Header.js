b246b466ea6b0d93b303a295245ca004ed6c130d
agnyasal <geasalsabila21@gmail.com>
2023-12-06 20:55:25 +0700

    fix header

--- app/home/components/Header.js
+++ app/home/components/Header.js
@@ -4,14 +4,12 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <section className="relative h-screen w-full overflow-hidden">
+    <section className="relative pt-20 h-screen w-full overflow-hidden">
       <Image
         src="/Header.jpg"
-        width={1500}
-        height={200}
+        layout="fill"
         objectFit="cover"
         alt="Background"
-        className="w-full"
       />
       <div className="absolute inset-0 flex flex-col justify-center items-start h-full p-10">
         <h1 className="text-5xl font-bold text-white mb-30">Born to explore</h1>
